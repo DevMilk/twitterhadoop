ebb5db0f8927dfb10a6ce9763507b1c0c36900ab
DevMilk <ugurkk2599@gmail.com>
2021-06-09 23:49:19 +0300

    delete unnecessary files

--- src/main/java/com/hadoop/twitterhadoop/all/dto/Settings.java
+++ src/main/java/com/hadoop/twitterhadoop/all/dto/Settings.java
@@ -17,7 +17,6 @@ public class Settings {
 
     public String dataset_path;
     public int number_of_top_users = 0;
-    public String date = "0";
     public int length = -1;
     public OPTIONS option = OPTIONS.RT; //tmp-length, mp-likes
 
